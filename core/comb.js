d0272fe7ebd754047c17eb098f5463903e1ee347
jjangjun <95jjangjun@gmail.com>
2022-11-14 13:48:55 +0900

    core: 조합 구하는 함수

---
+++ core/comb.js
@@ -0,0 +1,19 @@
+/**
+ *
+ * @param {any[]} arr
+ * @param {number} selectNumber
+ * @returns {any[] } combs array
+ */
+// 출처: https://velog.io/@devjade/JavaScript%EB%A1%9C-%EC%88%9C%EC%97%B4%EA%B3%BC-%EC%A1%B0%ED%95%A9-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0
+function getCombinations(arr, selectNumber) {
+  const results = [];
+  if (selectNumber === 1) return arr.map((value) => [value]);
+
+  arr.forEach((fixed, index, origin) => {
+    const rest = origin.slice(index + 1); // 해당하는 fixed를 제외한 나머지 뒤
+    const combinations = getCombinations(rest, selectNumber - 1); // 나머지에 대해서 조합을 구한다.
+    const attached = combinations.map((combination) => [fixed, ...combination]); //  돌아온 조합에 떼 놓은(fixed) 값 붙이기
+    results.push(...attached); // 배열 spread syntax 로 모두다 push
+  });
+  return results;
+}
