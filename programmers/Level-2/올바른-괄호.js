619b3fe7550510f0090452b29f2fd2c1a9cccad7
jjangjun <95jjangjun@gmail.com>
2022-12-23 13:45:06 +0900

    prog: 올바른괄호(L2)

--- programmers/Level-2/올바른-괄호.js
+++ programmers/Level-2/올바른-괄호.js
@@ -1,16 +1,16 @@
-// Level2 올바른 괄호
+// Level 2 올바른 괄호
 
+// 1. 배열 이용
 function solution(s) {
-  const stk = [];
-  const arr = Array.from(s);
   // 길이가 짝수가 아닐 때 or 첫 문자가 닫는 괄호일 때 올바른 괄호가 아님
   if (s.length % 2 !== 0 || arr[0] === ")") {
     return false;
   }
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === "(") {
-      stk.push(arr[i]);
+  const stk = [];
+  for (const c of s) {
+    if (c === "(") {
+      stk.push(c);
     } else {
       if (stk.length === 0) {
         return false;
@@ -19,9 +19,28 @@ function solution(s) {
     }
   }
 
-  if (stk.length === 0) {
-    return true;
-  } else {
+  return stk.length === 0;
+}
+
+// 2. 스택의 원리 이용
+
+function solution2(s) {
+  // 길이가 짝수가 아닐 때 or 첫 문자가 닫는 괄호일 때 올바른 괄호가 아님
+  if (s.length % 2 !== 0 || arr[0] === ")") {
     return false;
   }
+
+  let count = 0;
+  for (const c of s) {
+    if (c === "(") {
+      count++;
+    } else {
+      if (count === 0) {
+        return false;
+      }
+      count--;
+    }
+  }
+
+  return count === 0;
 }
