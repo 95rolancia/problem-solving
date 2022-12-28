94e08d7264900f72ab6e31eba25ac28f37ed3348
jjangjun <95jjangjun@gmail.com>
2022-12-28 18:11:06 +0900

    core: queue

---
+++ core/data-structure/queue.js
@@ -0,0 +1,104 @@
+class Node {
+  #value;
+  #next;
+  constructor(value) {
+    this.#value = value;
+    this.#next = null;
+  }
+
+  get value() {
+    return this.#value;
+  }
+
+  set value(v) {
+    this.#value = v;
+  }
+
+  get next() {
+    return this.#next;
+  }
+
+  set next(value) {
+    this.#next = value;
+  }
+}
+
+class Queue {
+  #head;
+  #tail;
+  #size;
+  constructor() {
+    this.#head = null;
+    this.#tail = null;
+    this.#size = 0;
+  }
+  시;
+  get size() {
+    return this.#size;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if (this.#head == null) {
+      this.#head = newNode;
+      this.#tail = newNode;
+    } else {
+      this.#tail.next = newNode;
+      this.#tail = newNode;
+    }
+
+    this.#size++;
+  }
+
+  dequeue() {
+    if (this.#head == null) {
+      throw new Error("q is empty");
+      return;
+    }
+    const value = this.#head.value;
+    this.#head = this.#head.next;
+    this.#size--;
+    return value;
+  }
+
+  peek() {
+    if (this.#head === null) {
+      throw new Error("q is empty");
+    }
+    return this.#head.value;
+  }
+
+  print() {
+    let result = "";
+    let cur = this.#head;
+    while (cur != null) {
+      result += `${cur.value}, `;
+      cur = cur.next;
+    }
+    console.log(result);
+  }
+}
+
+function solution(priorities, location) {
+  const q = new Queue();
+  priorities.forEach((priority, idx) => {
+    q.enqueue([priority, idx]);
+  });
+
+  let answer = 0;
+  priorities.sort((a, b) => b - a);
+
+  while (true) {
+    const [v, i] = q.peek();
+    if (priorities[answer] === v) {
+      q.dequeue();
+      answer++;
+      if (i === location) {
+        return answer;
+      }
+    } else {
+      q.enqueue(q.dequeue());
+    }
+  }
+}
