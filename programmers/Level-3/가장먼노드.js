28d7ec90892a12b7cf7ee3151324997f0483d461
jjangjun <95jjangjun@gmail.com>
2023-01-08 20:19:50 +0900

    prog: 입국심사(재풀이), 가장먼노드(재풀이)

--- programmers/Level-3/가장먼노드.js
+++ programmers/Level-3/가장먼노드.js
@@ -1,34 +1,53 @@
-// https://programmers.co.kr/learn/courses/30/lessons/49189
+// Level - 3 (https://programmers.co.kr/learn/courses/30/lessons/49189)
+// 핵심 키워드는 "노드", "간선", "최단경로"
+// 최단 경로가 제일 큰 경우의 집합을 구하는 문제
 
 function solution(n, edges) {
-  let answer = 0;
-
   const graph = Array.from({ length: n + 1 }, () => []);
 
-  edges.forEach((edge) => {
-    const [from, to] = edge;
+  edges.forEach(([from, to]) => {
     graph[from].push(to);
     graph[to].push(from);
   });
 
-  return bfs();
+  // 정점 1부터 각 정점까지의 거리를 저장하는 배열
+  const dis = Array(n + 1).fill(0);
+  const q = new Queue();
+  q.enqueue(1);
+  dis[1] = 1;
+  let maxDis = Number.MIN_SAFE_INTEGER;
 
-  function bfs() {
-    const visited = Array(n + 1).fill(-1);
-    const q = [1];
-    visited[1] = 0;
-    let maxDis = -1;
-
-    while (q.length > 0) {
-      const cur = q.shift();
-      for (const next of graph[cur]) {
-        if (visited[next] === -1) {
-          q.push(next);
-          visited[next] = visited[cur] + 1;
-          maxDis = Math.max(maxDis, visited[next]);
-        }
+  while (!q.isEmpty()) {
+    const cur = q.dequeue();
+    for (const next of graph[cur]) {
+      if (dis[next] === 0) {
+        q.enqueue(next);
+        dis[next] = dis[cur] + 1;
+        maxDis = Math.max(maxDis, dis[next]);
       }
     }
-    return visited.filter((dis) => dis === maxDis).length;
+  }
+  return dis.filter((d) => d === maxDis).length;
+}
+
+class Queue {
+  constructor() {
+    this.q = [];
+    this.front = 0;
+    this.rear = 0;
+  }
+
+  enqueue(value) {
+    this.q[this.rear++] = value;
+  }
+
+  dequeue() {
+    const value = this.q[this.front];
+    delete this.q[this.front];
+    this.front += 1;
+    return value;
+  }
+  isEmpty() {
+    return this.rear === this.front;
   }
 }
--- programmers/Level-3/입국심사.js
+++ programmers/Level-3/입국심사.js
@@ -1,9 +1,20 @@
-function solution(n, times) {
-  let answer = 0;
-  times.sort((a, b) => a - b);
+// 로그 시간 -> 이진 탐색
+// times -> 선형 로그 시간으로 충분히 가능
 
+// 우리는 특정 값을 찾는 것이 아니다.
+// 우리가 찾는 것은 최소 몇 분에 모든 심사가 끝나는가?
+// ㄴ 결정 문제 = 이진 탐색 = 파라메트릭 서치
+
+// 답은 최소 1분에서 10억분 * n 사이
+// 면접관들이 몇 명을 처리하는가?
+// 처리 가능한 입국자 n보다 작다면, 분을 올려야 하고
+// 입국자가 n보다 작다면 분을 낮춰야 한다.
+// 면접관이 시간대비 몇 명을 처리할 수 있는가 ?
+// 시간 / 심사시간 = 면접관 당 처리 가능한 입국자 수
+
+function solution(n, times) {
   let left = 0;
-  let right = n * times[times.length - 1];
+  let right = Math.max(...times) * n;
 
   while (left <= right) {
     let mid = Math.floor((left + right) / 2);
@@ -12,9 +23,8 @@ function solution(n, times) {
       left = mid + 1;
     } else {
       right = mid - 1;
-      answer = mid;
     }
   }
 
-  return answer;
+  return left;
 }
