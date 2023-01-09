81714a3b0109eee1f09d668960e17f487a770fe6
jjangjun <95jjangjun@gmail.com>
2023-01-09 18:51:58 +0900

    prog: 큰수만들기(재풀이, greedy)

--- programmers/Level-2/큰수만들기.js
+++ programmers/Level-2/큰수만들기.js
@@ -1,29 +1,21 @@
-console.log(solution('1000', 1));
+// 1 <= number <= 1,000,000 -> O(n) or O(n log n)으로 풀어야 함
 
 function solution(number, k) {
   let deletedCnt = 0;
-  let q = [];
+  const stk = [];
   
-  for (let i = 0; i < number.length; i++) {
-    if (q.length === 0) {
-      q.push(number[i]);
-    } else {
-      if (+q[q.length - 1] < +number[i]) {
-        while (+q[q.length - 1] < +number[i]) {
-          q.splice(q.length - 1, 1);
-          deletedCnt++;
-          if (deletedCnt === k) {
-            for (let j = i + 1; j < number.length; j++) {
-              q.push(number[j]);
-            }
-            return q.join("");
-          }
-        }
-        q.push(number[i]);
-      } else {
-        q.push(number[i]);
+  for(const nbr of number) {
+      while (deletedCnt < k && stk[stk.length - 1] < nbr) {
+          stk.pop();
+          deletedCnt += 1;
       }
-    }
+      stk.push(nbr);
   }
-  return q.join("").substring(0, q.length - k)
+  
+  while (deletedCnt < k) {
+      stk.pop();
+      deletedCnt += 1;
+  }    
+
+  return stk.join('');
 }
